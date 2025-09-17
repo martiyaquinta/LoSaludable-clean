fc9e59f900d11765accf828761490f7b5f86ec4c
Martina Yaquinta <martinayaquinta@Air-de-Martina.fibertel.com.ar>
2025-09-17 12:16:50 -0300

    modificamos App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -241,7 +241,7 @@ const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 
 
-            <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto"></div>
+            <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto">
             {/* Opciones saladas */}
             <div className="bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden">
               <div className="h-64 bg-gradient-to-br from-[#a1887f] to-[#81c784] flex items-center justify-center">
